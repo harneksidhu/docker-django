5562a4d518f629379e5fa28be75e80f54a135bd0
Harnek Sidhu <h6sidhu@gmail.com>
2015-11-28 15:30:33 -0500

    Removed data only container and included psql backup and restore script

--- scripts.sh
+++ scripts.sh
@@ -1,19 +1,19 @@
 echo $1
 
-if [ $1 == 'setup' ]
+if [ "$1" == "setup" ]
 
 then
   curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
-
-elif [ $1 == 'upgrade' ]
+elif [ "$1" == "upgrade" ]
 
 then 
-  docker run --rm --volumes-from dockerdjango_data_1 -v $(pwd):/backup busybox tar cvf /backup/backup.tar /var/lib/postgresql
+  docker exec ${PWD##*/}_postgres_1 pg_dump -U postgres -c -f /backup/output.tar -b -F tar postgres
   docker-compose kill
   docker-compose rm -v
   docker-compose up -d
-  docker run --rm --volumes-from dockerdjango_data_1 -v $(pwd):/backup busybox tar xvf /backup/backup.tar -C /var/lib/postgresql
-  rm backup.tar
+  sleep 5
+  docker exec ${PWD##*/}_postgres_1  pg_restore -U postgres -d postgres "/backup/output.tar"
+  rm output.tar
 
 fi
